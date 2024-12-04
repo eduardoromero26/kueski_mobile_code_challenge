96c7c47b4f276cac82a05d0909a52f1d125dd066
Edu <edu.roc26@gmail.com>
2024-12-04 14:46:08 -0600

    FEATURE: add env to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kueski_mobile_code_challenge/presentation/screens/home_screen.dart';
 
-void main() {
+void main()async  {
+    await dotenv.load(fileName: "assets/env/.env.dev");
   runApp(const MyApp());
 }
 
